c9de642253422aff78cc42f8ca5146c31b41280b
Ronaldo <ronaldo.rosa@bento.ifrs.edu.br>
2025-12-02 20:14:07 -0300

    Adicionei a rota/users

--- server.js
+++ server.js
@@ -1,20 +1,29 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
+// Rota de produtos
 app.get("/products", (req, res) => {
  res.json([
  { id: 1, name: "Produto Exemplo" },
  { id: 2, name: "Outro Produto" },
  ]);
 });
-// Nova funcionalidade
+// Rota de categorias
 app.get("/categories", (req, res) => {
  res.json([
  { id: 1, name: "Eletrônicos" },
  { id: 2, name: "Livros" },
  ]);
 });
-const PORT = 3000;
-app.listen(PORT, () => {
- console.log(`🚀 API Loja disponível em http://localhost:${PORT}/products`);
+// Rota de usuários
+app.get('/users', (req, res) => {
+ res.json([
+ { id: 1, name: 'Alice' },
+ { id: 2, name: 'Bob' },
+ { id: 3, name: 'Carlos' }
+ ]);
 });
+const PORT = 3000;
+app.listen(PORT, () => { console.log(`🚀 API Loja disponível em http://localhost:${PORT}/products`);
+});
+
